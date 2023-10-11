fda79539a1a97194f1cd2798445f8ed0e478fcd6
Lindorm236 <elielbjean@gmail.com>
2023-10-11 19:47:27 +0000

    second et troisième fetch

--- index.php
+++ index.php
@@ -28,4 +28,37 @@ echo"<br>";
 echo"<br>";
 var_dump($data1);
 
+echo"<br>";
+echo"<br>";
+
+$result2 =mysqli_query($conn, "SELECT * FROM user WHERE id=1");
+
+//avec fetch_row : tableau indexé permet de recuperer des données 
+$data2 = mysqli_fetch_row($result2);
+
+echo"<br>";
+echo"Second fetch";
+echo"<br>";
+echo"<br>";
+var_dump($result2);
+echo"<br>";
+echo"<br>";
+var_dump($data2);
+
+
+$result3 = mysqli_query($conn, "SELECT user_name, email, id FROM user WHERE id=1");
+$data3 = mysqli_fetch_assoc($result3);
+
+
+
+echo"<br>";
+echo"Troisième fetch";
+echo"<br>";
+echo"<br>";
+var_dump($result3);
+echo"<br>";
+echo"<br>";
+var_dump($data3);
+
+
 ?>
